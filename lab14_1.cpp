3a6ba0ab2e7deb805abb1f0ac22e468c8f6922f9
Your Name <you@example.com>
2023-01-13 16:57:16 +0700

    Full

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,47 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double a[] , int n , double b[]){
+    int i;
+    double sum;
+    for(i=0;i<n;i++){
+        sum+=a[i];
+    }
+    sum = sum/n;
+    b[0]=sum;
+    
+    float sd;
+    double u;
+    for(i=0;i<n;i++){
+        u += pow(a[i],2);
+    }
+        double e = u/n;
+        sd = sqrt(e-(pow(sum,2)));
+        b[1]=sd;
+    
+    float gm;
+    float g=1;
+    for(i=0;i<n;i++){
+        g *= a[i];
+    }
+        gm = pow(g,(float)1/n);
+        b[2]=gm;
+    
+    float hm;
+    float k=0;
+    for(i=0;i<n;i++){
+        k += 1/a[i];
+    }
+        hm = n/k;
+        b[3]=hm;
+        
+        
+    double max = a[0] , min = a[0];
+    for(i=1;i<n;i++){
+        if(a[i]>max) max=a[i];
+    b[4]=max;
+        if(a[i]<min) min=a[i];
+    b[5]=min;
+    }
+}
